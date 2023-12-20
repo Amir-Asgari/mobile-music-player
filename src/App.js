b5f87f47ec2b8cd9138ef16191628bfd67b82c32
Amir Asgari <Mr.a.asgari@gmail.com>
2023-12-21 00:02:27 +0330

    initail again

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Player from "./Player";
+import './App.css';
+import '../public/service-worker'
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <p>آهنگ های شاد عروسی</p>
+      </div>
       <Player />
     </div>
   );
--- src/Player.js
+++ src/Player.js
@@ -1,45 +1,63 @@
 import React, { useState } from 'react';
 import AudioPlayer from 'react-audio-player';
 import songsData from './songs.json';
+import './Player.css'
+import SongSlider from './componnets/SongSlider';
 
 const Player = () => {
-  const [currentSong, setCurrentSong] = useState(0);
+    // const [currentSong, setCurrentSong] = useState(0);
 
-  const playSong = () => {
-    console.log("Song is playing");
-  };
+    // const playSong = () => {
+    //     console.log("Song is playing");
+    // };
 
-  const pauseSong = () => {
-    console.log("Song is paused");
-  };
+    // const pauseSong = () => {
+    //     console.log("Song is paused");
+    // };
 
-  const nextSong = () => {
-    setCurrentSong((prevSong) => (prevSong + 1) % songs.length);
-  };
+    // const nextSong = () => {
+    //     setCurrentSong((prevSong) => (prevSong + 1) % songs.length);
+    // };
 
-  const prevSong = () => {
-    setCurrentSong((prevSong) => (prevSong - 1 + songs.length) % songs.length);
-  };
+    // const prevSong = () => {
+    //     setCurrentSong((prevSong) => (prevSong - 1 + songs.length) % songs.length);
+    // };
 
-  const { playlists } = songsData;
-  const songs = playlists[0].songs;
+    const { playlists } = songsData;
+    const songs = playlists[0].songs;
 
-  return (
-    <div>
-      <AudioPlayer
-        autoPlay
-        src={songs[currentSong].url}
-        onPlay={playSong}
-        onPause={pauseSong}
-        // متد‌های دیگر مانند onEnded و ...
-      />
-      <h1>{songs[currentSong].title}</h1>
-      <h2>{songs[currentSong].artist}</h2>
-      <img src={songs[currentSong].image} alt={songs[currentSong].artist} />
-      <button onClick={prevSong}>Previous</button>
-      <button onClick={nextSong}>Next</button>
-    </div>
-  );
+    return (
+        <div>
+            <SongSlider songs={songs} />
+
+            {/* <AudioPlayer
+                autoPlay
+                src={songs[currentSong].url}
+                onPlay={playSong}
+                onPause={pauseSong}
+                onEnded={nextSong}
+            // متد‌های دیگر مانند onEnded و ...
+            />
+            <h1>{songs[currentSong].title}</h1>
+            <h2>{songs[currentSong].artist}</h2>
+            <div style={{ display: 'flex' , justifyContent:"center" }}>
+                <img 
+                style={{ borderRadius: "50%"}}
+                src={songs[currentSong].image} alt={songs[currentSong].artist} />
+            </div> */}
+{/* 
+            <span style={{ display: 'flex', justifyContent: "center" }}>
+                <button onClick={prevSong}>Previous</button>
+                <button onClick={playSong}>Play</button>
+                <button onClick={pauseSong}>Pause</button>
+                <button onClick={nextSong}>Next</button>
+            </span> */}
+            {/* <div className="dance">
+                <img src="https://media2.giphy.com/media/XMaB779YCmP9m/giphy.gif" alt="Dance GIF" />
+            </div>
+            <p>Enjoy the dance!</p> */}
+        </div>
+    );
 };
 
 export default Player;
