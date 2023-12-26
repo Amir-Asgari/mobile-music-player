6b0b20cfe14002ba0624779a2c0e5ed183e6fe4d
Amir Asgari <Mr.a.asgari@gmail.com>
2023-12-27 00:32:12 +0330

    add routes to the project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import Slider from "react-slick";
 import Player from "./Player";
-import './App.css';
-import '../public/service-worker'
+import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import AdvertisingBox from "./componets/AdvertisingBox/AdvertisingBox";
+import { BrowserRouter as Router, Route, Switch , Routes, BrowserRouter } from "react-router-dom";
+import AlbumList from "./componets/Album/AlbumList";
+import albumsData from "./songs.json";
+// import './service-worker'
 
-function App() {
+function App({ sliderData }) {
+  const [playlist, setPlayList] = useState();
+
+  // if (!Array.isArray(sliderData) || sliderData.length <= 0) {
+  //   return null;
+  // }
+
+  // const GalleryPrevArrow = ({ currentSlide, slideCount, ...props }) => {
+  //   const { className, onClick } = props;
+
+  //   return (
+  //     <div {...props} className="custom-prevArrow" onClick={onClick}>
+  //       <svg
+  //         xmlns="http://www.w3.org/2000/svg"
+  //         width="24"
+  //         height="24"
+  //         viewBox="0 0 24 24"
+  //       >
+  //         <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+  //       </svg>
+  //     </div>
+  //   );
+  // };
+  // const GalleryNextArrow = ({ currentSlide, slideCount, ...props }) => {
+  //   const { className, onClick } = props;
+
+  //   return (
+  //     <div {...props} className="custom-nextArrow" onClick={onClick}>
+  //       <svg
+  //         xmlns="http://www.w3.org/2000/svg"
+  //         width="24"
+  //         height="24"
+  //         viewBox="0 0 24 24"
+  //       >
+  //         <path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" />
+  //       </svg>
+  //     </div>
+  //   );
+  // };
+
+  // const sliderSettings = {
+  //   className: "center",
+  //   centerMode: true,
+  //   // centerPadding: "40px",
+  //   infinite: true,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   arrows: true,
+  //   speed: 500,
+  //   slidesPerRow: 1,
+  //   dots: true,
+  //   nextArrow: <GalleryNextArrow />,
+  //   prevArrow: <GalleryPrevArrow />,
+  // };
+// console.log(albumsData.playlists);
   return (
-    <div className="App">
-      <div>
-        <p>آهنگ های شاد عروسی</p>
-      </div>
-      <Player />
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<AlbumList albums={albumsData} />}/>
+          <Route path="/album/:id" element={<AlbumList albums={albumsData} />}/>
+      </Routes>
+    </BrowserRouter>
+    );
 }
 
 export default App;
--- src/Player.js
+++ src/Player.js
@@ -2,60 +2,15 @@ import React, { useState } from 'react';
 import AudioPlayer from 'react-audio-player';
 import songsData from './songs.json';
 import './Player.css'
-import SongSlider from './componnets/SongSlider';
+import SongSlider from './componets/SongSlider';
 
-const Player = () => {
-    // const [currentSong, setCurrentSong] = useState(0);
-
-    // const playSong = () => {
-    //     console.log("Song is playing");
-    // };
-
-    // const pauseSong = () => {
-    //     console.log("Song is paused");
-    // };
-
-    // const nextSong = () => {
-    //     setCurrentSong((prevSong) => (prevSong + 1) % songs.length);
-    // };
-
-    // const prevSong = () => {
-    //     setCurrentSong((prevSong) => (prevSong - 1 + songs.length) % songs.length);
-    // };
-
-    const { playlists } = songsData;
+const Player = ({selectedAlbum}) => {
+      const { playlists } = songsData;
     const songs = playlists[0].songs;
 
     return (
         <div>
-            <SongSlider songs={songs} />
-
-            {/* <AudioPlayer
-                autoPlay
-                src={songs[currentSong].url}
-                onPlay={playSong}
-                onPause={pauseSong}
-                onEnded={nextSong}
-            // متد‌های دیگر مانند onEnded و ...
-            />
-            <h1>{songs[currentSong].title}</h1>
-            <h2>{songs[currentSong].artist}</h2>
-            <div style={{ display: 'flex' , justifyContent:"center" }}>
-                <img 
-                style={{ borderRadius: "50%"}}
-                src={songs[currentSong].image} alt={songs[currentSong].artist} />
-            </div> */}
-{/* 
-            <span style={{ display: 'flex', justifyContent: "center" }}>
-                <button onClick={prevSong}>Previous</button>
-                <button onClick={playSong}>Play</button>
-                <button onClick={pauseSong}>Pause</button>
-                <button onClick={nextSong}>Next</button>
-            </span> */}
-            {/* <div className="dance">
-                <img src="https://media2.giphy.com/media/XMaB779YCmP9m/giphy.gif" alt="Dance GIF" />
-            </div>
-            <p>Enjoy the dance!</p> */}
+            <SongSlider songs={selectedAlbum} />
         </div>
     );
 };
