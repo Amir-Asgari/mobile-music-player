3d4ba1f86b1e8b78b8f20d59dfbef8790f3a785b
Amir Asgari <Mr.a.asgari@gmail.com>
2023-12-27 00:38:00 +0330

    Player.js --solve conflict

--- src/Player.js
+++ src/Player.js
@@ -2,11 +2,7 @@ import React, { useState } from 'react';
 import AudioPlayer from 'react-audio-player';
 import songsData from './songs.json';
 import './Player.css'
-<<<<<<< HEAD
 import SongSlider from './componets/songSlider/SongSlider';
-=======
-import SongSlider from './componets/SongSlider';
->>>>>>> 4fffe9c2f4fe57f32a13135484e5c74284bca4d3
 
 const Player = ({selectedAlbum}) => {
       const { playlists } = songsData;
