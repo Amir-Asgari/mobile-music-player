7a3b3c028975074d094d36fce3a549125cbc3cc0
Amir Asgari <Mr.a.asgari@gmail.com>
2023-12-28 06:29:00 +0330

    likedButton.js --init component

--- src/componets/Album/AlbumList.js
+++ src/componets/Album/AlbumList.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const AlbumList = () => {
   return (
     <div>
-      Album list
-    </div>
+     <div>
+      <div>
+        <image src={image}/>
+      </div>
+      <div>
+        <h3>{title}</h3>
+        <h4>{artist}</h4>
+      </div>
+     </div>
+     <div>
+
+     </div>
+     </div>
   )
 }
 
---
+++ src/componets/MUI/LikeButton/LikeButton.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+const LikeButton = () => {
+    const [liked, setliked] = useState(false)
+
+    const handleClickLike =()=>{
+        setliked(!liked)
+    }
+  return (
+    <div>
+        <IconButton onClick={handleClickLike} color={liked? 'error': 'default'}>
+            <FavoriteIcon/>
+        </IconButton>
+    </div>
+  )
+}
+
+export default LikeButton
